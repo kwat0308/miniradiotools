960a819690aa3e1248e5a7ac2a261ed02e5a4bb4
lguelzow <lukas.guelzow@kit.edu>
2023-08-10 12:16:58 +0200

    Incorporated optional input in Auger coordinates new input parameter "Auger_input" True - your input is in Auger coordinates False - your input is in Corsika coordinates (optional rotation matrix of -90 degrees around z-axis applied)

--- starshapes.py
+++ starshapes.py
@@ -13,6 +13,7 @@ import sys
 def create_stshp_list(zenith, azimuth, filename="antenna.list", 
                         obslevel=156400.0, # default for Dunhuang, !!in cm!!
                         obsplane = "gp",
+                        Auger_input = False, 
                         inclination=61.60523, # default for Dunhuang (in degrees)
                         Rmin=0., Rmax=50000., n_rings=20, # for positions in starshape !!in cm!!
                         arm_orientations=np.deg2rad([0, 45, 90, 135, 180, 225, 270, 315]), # for positions in starshape (in degrees)
@@ -48,13 +49,28 @@ def create_stshp_list(zenith, azimuth, filename="antenna.list",
     Rmin, Rmax, n_rings, arm_orientations : used to calculate the positions of the antennas on the arms of the starshape !!in cm!!
             Do not change unless you know what you are doing!
     """
+
     print(f"Generating antenna positions in {obsplane} at {obslevel} cm.")
     print(f"zenith: {zenith} degrees - in Corsika convention")
     print(f"azimuth: {azimuth} degrees - in Corsika convention")
 
     # convert to rad for numpy calculations, change azimuth to Auger convention (for radiotools)
     zenith = np.deg2rad(zenith)
-    azimuth = np.deg2rad(azimuth + 270) # add 270 degrees for Auger conventions
+    azimuth = np.deg2rad(azimuth) # add 270 degrees for Auger conventions
+
+    # define angle for Auger rotation 
+    # set as 0 degrees if you want normal Corsika input
+    if Auger_input == True:
+          rot_angle = np.deg2rad(270) # Auger coordinates are Corsika coordinates rotated by -90 degrees
+    else:
+          rot_angle = 0
+
+    # rotation matrix for transformation between Auger and Corsika coordinate system
+    # rotation matrix for rotation around z-axis
+    Rotation_z_axis = np.array([[np.cos(rot_angle),  (-1) * np.sin(rot_angle), 0], \
+                      [np.sin(rot_angle), np.cos(rot_angle), 0], \
+                      [0, 0, 1]])
+
 
     # definition of inclination and declination are in coordtransform.py
     inclination = np.deg2rad(inclination) # default value is for Dunhuang
@@ -86,50 +102,59 @@ def create_stshp_list(zenith, azimuth, filename="antenna.list",
     with open(filename, "w") as file:
         for i in np.arange(1, n_rings + 1): # loop over number of antenna rings
                 for j in np.arange(len(arm_orientations)): # loop over number of arms
+                        # generate station positions in shower plane coordinates
                         station_position = rs[i] * spherical_to_cartesian(np.pi * 0.5, arm_orientations[j])
                         name = "pos_%i_%i_%.0f_%s" % (rs[i], np.rad2deg(arm_orientations[j]), obslevel, obsplane)
 
                         # ground plane:
                         if obsplane == "gp":
-                                pos_2d = cst.transform_from_vxB_vxvxB_2D(station_position)  # position if height in observer plane should be zero
+                                # transform station positions to ground plane coordinates and set all the z coordinates to 0
+                                pos_2d = cst.transform_from_vxB_vxvxB_2D(station_position)
                                 # pos_2d[0] += dx
                                 # pos_2d[1] += dy
                                 
-                                # two versions!
-                                # keep the same coordinate system and see proper starshapes in antenna_plotter
-                                x, y, z = pos_2d[0], pos_2d[1], obslevel
-                                # change coordinate system from Auger to Corsika
-                                # x, y, z = pos_2d[1], (-1) * pos_2d[0], obslevel
+                                # write transformed coordinates into kartesian vector and 
+                                # set z coordinate to observation level
+                                gp_position = np.array([pos_2d[0], pos_2d[1], obslevel])
 
-                                station_positions_groundsystem.append([x, y, z])
+                                # apply rotation matrix to stations
+                                # Corsika input will stay the same, Auger input will be rotated by -90 degrees
+                                gp_position = np.dot(Rotation_z_axis, gp_position)
+
+                                # write all station positions into list for later conversion to shower plane coordinates
+                                station_positions_groundsystem.append(gp_position)
 
                                 # save the generated starshapes to the antenna.list file
                                 # positions in cm
-                                file.write(f"AntennaPosition = {x} {y} {z} {name}\n")
+                                file.write(f"AntennaPosition = {gp_position[0]} {gp_position[1]} {gp_position[2]} {name}\n")
 
                         # shower plane:
                         elif obsplane == "sp":
+                                # transform station positions to ground plane coordinates
                                 pos = cst.transform_from_vxB_vxvxB(station_position)
                                 # pos[0] += dx
                                 # pos[1] += dy
 
-                                # two versions!
-                                # keep the same coordinate system and see proper starshapes in antenna_plotter
-                                x, y, z = pos[0], pos[1], (pos[2] + obslevel)
-                                # change coordinate system from Auger to Corsika
-                                # x, y, z = pos[1], (-1) * pos[0], (pos[2] + obslevel)
+                                # write transformed coordinates into kartesian vector and 
+                                # add observation level to z coordinate
+                                sp_position = np.array([pos[0], pos[1], (pos[2] + obslevel)])
 
-                                station_positions_groundsystem.append([x, y, z])
+                                # apply rotation matrix to stations
+                                # Corsika input will stay the same, Auger input will be rotated by -90 degrees
+                                sp_position = np.dot(Rotation_z_axis, sp_position)
+
+                                # write all station positions into list for later conversion to shower plane coordinates
+                                station_positions_groundsystem.append([sp_position])
 
                                 # save the generated starshapes to the antenna.list file
                                 # positions in cm
-                                file.write(f"AntennaPosition = {x} {y} {z} {name}\n")
+                                file.write(f"AntennaPosition = {sp_position[0]} {sp_position[1]} {sp_position[2]} {name}\n")
                         
                         # dealing with wrong obsplanes:
                         else:
                                 sys.exit("Wrong choice of observation plane. Possible options are 'gp' or 'sp'. \n Quitting...")
 
-        print(np.array(station_positions_groundsystem[0:10]))
+        # print(np.array(station_positions_groundsystem[0:10]))
         print("Saved antenna positions (in cartesian coordinates) to file: ", filename)
 
 
