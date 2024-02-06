e189c5d3f511f7366bffbdcfe6d26cc10ba3d445
lguelzow <lukas.guelzow@kit.edu>
2024-02-06 14:39:05 +0100

    experiment with multiple directory search

--- biohazard_do_not_open/analysis.py
+++ biohazard_do_not_open/analysis.py
@@ -99,7 +99,7 @@ if __name__ == '__main__':
     else:
         print(f"Searching directory {options.directory} for .reas files")
         # find .reas files with glob
-        reas_names = glob.glob(options.directory + "/**" + "/SIM??????.reas") 
+        reas_names = glob.glob(options.directory + "/**/**" + "/SIM??????.reas") 
         # use ** iif you want to go through all subdirectories, use * if you want to go only one level deeper
         print(f"Found {len(reas_names)} showers to plot!")
         # loop over all reas files
